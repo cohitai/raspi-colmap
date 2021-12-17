5e795785cddb47f4f138395d3662104d66779650
cohitai <cohitai@gmail.com>
2021-12-17 14:46:59 +0100

    BUG: stucks at poisson - cant open PLY file

--- main.py
+++ main.py
@@ -20,6 +20,15 @@ COLMAP_OUTPUT_DIR = DATA_DIR + "/d3"
 
 # frame's size:
 HT, WH = 720, 1080
+
+# set PATHS for colmap (to be added later to .env)
+LOCAL_SERVER_DIR = '/home/liteandfog/raspi-colmap'
+SERVER_RAW_DIR = LOCAL_SERVER_DIR + '/data/d1'
+SERVER_MASKED_DIR = LOCAL_SERVER_DIR + '/data/d2'
+SERVER_COLMAP_OUTPUT_DIR = LOCAL_SERVER_DIR + '/data/d3'
+
+
+# BUG: stucks at poisson - cant open PLY file
 # for the Dockerized application: 2 containers app:
 #                1. principal container raspi/colmap with these files.
 #                2. colmap:test container
@@ -27,7 +36,7 @@ HT, WH = 720, 1080
 #                docker daemon local to docker daemon in container (1).
 #                /data/ local to /data/ in container (1).
 #                /data/ local to /data/ in container (2).
-# running with:
+# running with: docker run -v /var/run/docker.sock:/var/run/docker.sock  -v /home/liteandfog/raspi-colmap/data:/app/data --runtime=nvidia raspi/reconstructor:latest
 
 
 
@@ -43,7 +52,7 @@ def main():
     extractor.create_mask(height=HT, width=WH, apply_mask=True, rescale=True, save_to_local=True, plot=False)
 
     # reconstruct
-    colmap_client = DockerizedColmap('/home/liteandfog/raspi-colmap/data/d1', '/home/liteandfog/raspi-colmap/data/d2', '/home/liteandfog/raspi-colmap/data/d3')
+    colmap_client = DockerizedColmap(SERVER_RAW_DIR, SERVER_MASKED_DIR, SERVER_COLMAP_OUTPUT_DIR)
     colmap_client.reconstruct(outliers=True, poisson=True)
 
     # upload to azure
