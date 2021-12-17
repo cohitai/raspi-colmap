358d4b95e720028bcf3b1e083409ea5f2a781550
cohitai <cohitai@gmail.com>
2021-12-17 14:09:20 +0100

    redeisgn volume shares

--- main.py
+++ main.py
@@ -17,8 +17,18 @@ RAW_DATA_DIR = DATA_DIR + "/d1"
 MASKED_DATA_DIR = DATA_DIR + "/d2"
 COLMAP_OUTPUT_DIR = DATA_DIR + "/d3"
 
+
 # frame's size:
 HT, WH = 720, 1080
+# for the Dockerized application: 2 containers app:
+#                1. principal container raspi/colmap with these files.
+#                2. colmap:test container
+#                shared volumes:
+#                docker daemon local to docker daemon in container (1).
+#                /data/ local to /data/ in container (1).
+#                /data/ local to /data/ in container (2).
+# running with:
+
 
 
 def main():
@@ -33,7 +43,7 @@ def main():
     extractor.create_mask(height=HT, width=WH, apply_mask=True, rescale=True, save_to_local=True, plot=False)
 
     # reconstruct
-    colmap_client = DockerizedColmap(RAW_DATA_DIR, MASKED_DATA_DIR, COLMAP_OUTPUT_DIR)
+    colmap_client = DockerizedColmap('/home/liteandfog/raspi-colmap/data/d1', '/home/liteandfog/raspi-colmap/data/d2', '/home/liteandfog/raspi-colmap/data/d3')
     colmap_client.reconstruct(outliers=True, poisson=True)
 
     # upload to azure
