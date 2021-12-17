107587a709169365ab6e9b2c85a816c0fed9706a
cohitai <cohitai@gmail.com>
2021-12-17 11:27:34 +0100

    uncomment

--- main.py
+++ main.py
@@ -24,16 +24,16 @@ def main():
     # clean the working directory
     extractor.init()
     # dl last container to local
-    # container_name = extractor.fetch_last_container()
+    container_name = extractor.fetch_last_container()
     # apply mask and save to local
-    # extractor.create_mask(height=720, width=1080, apply_mask=True, rescale=True, save_to_local=True, plot=False)
+    extractor.create_mask(height=720, width=1080, apply_mask=True, rescale=True, save_to_local=True, plot=False)
 
     # reconstruct
-    # colmap_client = DockerizedColmap(RAW_DATA_DIR, MASKED_DATA_DIR, COLMAP_OUTPUT_DIR)
-    # colmap_client.reconstruct(outliers=True, poisson=True)
+    colmap_client = DockerizedColmap(RAW_DATA_DIR, MASKED_DATA_DIR, COLMAP_OUTPUT_DIR)
+    colmap_client.reconstruct(outliers=True, poisson=True)
 
     # upload to azure
-    # FlushAzure(COLMAP_OUTPUT_DIR, container_name).flush()
+    FlushAzure(COLMAP_OUTPUT_DIR, container_name).flush()
 
 
 if __name__ == '__main__':
