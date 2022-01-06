a9cb30070c344ab4d18dcb542528a9abe635bd59
cohitai <cohitai@gmail.com>
2022-01-06 15:55:14 +0100

    new supplements.py module

--- reconstruction.py
+++ reconstruction.py
@@ -23,16 +23,12 @@ class DockerizedColmap:
         self.masked_dir = masked_dir
         self.output_dir = output_dir
         self.input_dense = output_dir + '/dense.ply'
-        self.output_inlier_cloud = output_dir + '/dense_inlier.ply'
-        self.output_poisson = output_dir + '/poisson.ply'
 
         if not os.path.exists(self.output_dir):
             os.makedirs(self.output_dir)
 
-    def reconstruct(self, outliers=False, poisson=False):
-        """ COLMAP in docker
-           :params: boolean; outliers, adds the outliers feature,
-                    boolean; poisson, adds Poisson meshing. """
+    def reconstruct(self):
+        """ COLMAP in docker """
 
         client = docker.from_env()  # connect to docker daemon
 
@@ -71,22 +67,6 @@ class DockerizedColmap:
 
         self._run_colmap(client, colmap_cmds, mount_dict, self.DATASET_PATH)
 
-        """outliers removal"""
-        if outliers:
-            pcd = o3d.io.read_point_cloud(self.input_dense)
-            inlier_cloud, outlier_cloud = self._remove_outliers(pcd, self._create_outliers_list(pcd))
-            logging.info(f"number of points removed by knn:{len(pcd.points) - len(inlier_cloud.points)}")
-            #  save to file
-            o3d.io.write_point_cloud(self.output_inlier_cloud, inlier_cloud, write_ascii=True, compressed=False,
-                                     print_progress=False)
-
-        """poisson meshing"""
-        if poisson:
-            pcd = o3d.io.read_point_cloud(self.output_inlier_cloud) if Path(self.output_inlier_cloud).is_file() \
-                else o3d.io.read_point_cloud(self.input_dense)
-            poisson_mesh = self.poisson_reconstruction(pcd)
-            o3d.io.write_triangle_mesh(self.output_poisson, poisson_mesh, write_ascii=True, compressed=False)
-
     @staticmethod
     def _run_colmap(client, cmd_dict, mount_dict, wd):
         """ dockerized COLMAP"""
@@ -98,22 +78,3 @@ class DockerizedColmap:
         for colmap_command, command in cmd_dict.items():
             logging.info(f"executing colmap: {colmap_command}")
             _execute_colmap_command(client, command, mount_dict, wd)
-
-    @staticmethod
-    def _create_outliers_list(pcd):
-        # Convert open3d format to numpy array
-        pcd_colors = np.asarray(pcd.colors) * 255
-        pcd_colors_summed = np.expand_dims(pcd_colors.sum(axis=1), axis=1)
-        return np.where(np.any(pcd_colors_summed > 720, axis=1))[0].tolist()
-
-    @staticmethod
-    def _remove_outliers(cloud, ind):
-        inlier_cloud = cloud.select_by_index(ind, invert=True)
-        outlier_cloud = cloud.select_by_index(ind, invert=False)
-        return inlier_cloud.remove_radius_outlier(nb_points=100, radius=0.1)[0], outlier_cloud
-
-    @staticmethod
-    def poisson_reconstruction(pcd):
-        return o3d.geometry.TriangleMesh.create_from_point_cloud_poisson(pcd,
-                                                                         depth=12, width=0, scale=1.1,
-                                                                         linear_fit=False)[0]
---
+++ supplements.py
@@ -0,0 +1,58 @@
+import logging
+import open3d as o3d
+import numpy as np
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()  # take environment variables from .env.
+from azure.storage.blob import BlobServiceClient, AccessPolicy, ContainerSasPermissions, PublicAccess
+
+
+class FlushAzure:
+
+    """class to implement poisson reconstruction and outlier removal"""
+
+    def __init__(self, container_name, output_dir):
+        self.container_name = container_name
+        self.output_dir = output_dir
+        self.input_dense = output_dir + '/dense.ply'
+        self.output_inlier_cloud = output_dir + '/dense_inlier.ply'
+        self.output_poisson = output_dir + '/poisson.ply'
+
+    def reconstruct(self, outliers=False, poisson=False):
+
+        """outliers removal"""
+        if outliers:
+            pcd = o3d.io.read_point_cloud(self.input_dense)
+            inlier_cloud, outlier_cloud = self._remove_outliers(pcd, self._create_outliers_list(pcd))
+            logging.info(f"number of points removed by knn:{len(pcd.points) - len(inlier_cloud.points)}")
+            #  save to file
+            o3d.io.write_point_cloud(self.output_inlier_cloud, inlier_cloud, write_ascii=True, compressed=False,
+                                     print_progress=False)
+
+        """poisson meshing"""
+        if poisson:
+            pcd = o3d.io.read_point_cloud(self.output_inlier_cloud) if Path(self.output_inlier_cloud).is_file() \
+                else o3d.io.read_point_cloud(self.input_dense)
+            poisson_mesh = self.poisson_reconstruction(pcd)
+            o3d.io.write_triangle_mesh(self.output_poisson, poisson_mesh, write_ascii=True, compressed=False)
+
+    @staticmethod
+    def _create_outliers_list(pcd):
+        # Convert open3d format to numpy array
+        pcd_colors = np.asarray(pcd.colors) * 255
+        pcd_colors_summed = np.expand_dims(pcd_colors.sum(axis=1), axis=1)
+        return np.where(np.any(pcd_colors_summed > 720, axis=1))[0].tolist()
+
+    @staticmethod
+    def _remove_outliers(cloud, ind):
+        inlier_cloud = cloud.select_by_index(ind, invert=True)
+        outlier_cloud = cloud.select_by_index(ind, invert=False)
+        return inlier_cloud.remove_radius_outlier(nb_points=100, radius=0.1)[0], outlier_cloud
+
+    @staticmethod
+    def poisson_reconstruction(pcd):
+        return o3d.geometry.TriangleMesh.create_from_point_cloud_poisson(pcd,
+                                                                         depth=12, width=0, scale=1.1,
+                                                                         linear_fit=False)[0]
+
