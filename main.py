d53b262c7fa39f34934ed8e29656db83bbf60828
cohitai <cohitai@gmail.com>
2022-01-20 10:43:26 +0100

    tdd: ok ,parser is added

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import time
-
+import argparse
 from preprocess import MaskAzure
 from reconstruction import DockerizedColmap
 from postprocess import FlushAzure
@@ -22,35 +22,55 @@ SUPP_OUTPUT_DIR = DATA_DIR + "/d4"
 
 
 def main():
-    # preprocess (1)
-    extractor = MaskAzure(RAW_DATA_DIR, MASKED_DATA_DIR, COLMAP_OUTPUT_DIR, SUPP_OUTPUT_DIR)
-    # clean the working directory
-    extractor.init()
-    # dl last container to local
-    container_name = extractor.fetch_last_container()
-    # debug_container=["1639656186-281117"]
-    # apply mask and save to local (2)
-    extractor.create_mask(height=720, width=1080, hsv_params=((0, 51, 0), (179, 255, 255)), dilate_iter=3, dilate_ker=3,
-                          erode_iter=3, erode_ker=6, apply_mask=True, rescale=True, save_to_local=True, plot=False)
 
-    # reconstruct (3)
-    colmap_client = DockerizedColmap(RAW_DATA_DIR, MASKED_DATA_DIR, COLMAP_OUTPUT_DIR)
-    colmap_client.reconstruct()
+    parser = argparse.ArgumentParser(description="Raspi colmap 3D reconstruction.")
+    parser.add_argument("-A", "--automate", help="automate server by time", nargs='+', default=10000, type=int)
+    parser.add_argument("-M", "--mask", help="mask parameters", nargs='+', type=int)
 
-    # upload to azure
-    flusher = FlushAzure(COLMAP_OUTPUT_DIR, container_name)
-    flusher.flush()
+    args = parser.parse_args()
 
-    # download dense.ply to local and apply post-operations (4)
-    PcdOps(SUPP_OUTPUT_DIR, container_name).fetch_dense_from_remote()
-    PcdOps(SUPP_OUTPUT_DIR, container_name).reconstruct(outliers=True, poisson=True)
+    if args.automate:
+        while True:
 
-    # flush new files to container
-    flusher.flush_from([SUPP_OUTPUT_DIR + "/" + "dense_inlier.ply", SUPP_OUTPUT_DIR + "/" + "poisson.ply"])
+            # preprocess (1)
+            logging.info("STARTING AUTOMATION:")
+            extractor = MaskAzure(RAW_DATA_DIR, MASKED_DATA_DIR, COLMAP_OUTPUT_DIR, SUPP_OUTPUT_DIR)
+            # clean the working directory
+            logging.info("Clean the workspace.")
+            extractor.init()
+            # dl last container to local
+            logging.info("Extracting data from Azure.")
+            container_name = extractor.fetch_last_container()
+            # debug_container=["1639656186-281117"]
+            # apply mask and save to local (2)
+            logging.info("Applying Mask on data")
+            extractor.create_mask(height=720, width=1080, hsv_params=((0, 51, 0), (179, 255, 255)), dilate_iter=3,
+                                  dilate_ker=3, erode_iter=3, erode_ker=6, apply_mask=True, rescale=True,
+                                  save_to_local=True, plot=False)
+
+            # reconstruct (3)
+            logging.info("COLMAP reconstruction:")
+            colmap_client = DockerizedColmap(RAW_DATA_DIR, MASKED_DATA_DIR, COLMAP_OUTPUT_DIR)
+            colmap_client.reconstruct()
+
+            # upload to azure
+            logging.info("Upload to Azure:")
+            flusher = FlushAzure(COLMAP_OUTPUT_DIR, container_name)
+            flusher.flush()
+
+            # download dense.ply to local and apply post-operations (4)
+            logging.info("Post-process with open3d")
+            PcdOps(SUPP_OUTPUT_DIR, container_name).fetch_dense_from_remote()
+            PcdOps(SUPP_OUTPUT_DIR, container_name).reconstruct(outliers=True, poisson=True)
+
+            # flush new files to container
+            logging.info("Uploading to Azure, poisson and inlier point cloud.")
+            flusher.flush_from([SUPP_OUTPUT_DIR + "/" + "dense_inlier.ply", SUPP_OUTPUT_DIR + "/" + "poisson.ply"])
+
+            # go to pause
+            logging.info("Going to sleep..")
+            time.sleep(args.automate[0])
 
 
 if __name__ == '__main__':
-    while True:
-        main()
-        logging.info("going to sleep..")
-        time.sleep(10000)
+    main()
